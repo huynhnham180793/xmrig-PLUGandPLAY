7ee914a83592cb21257bacec73f301a7cddaa9e2
huynhnham180793 <48785801+huynhnham180793@users.noreply.github.com>
2019-03-28 09:39:37 +0700

    Update start_proxy.sh

--- start_proxy.sh
+++ start_proxy.sh
@@ -5,7 +5,7 @@
 
 if [ ! -f key.pem ] || [ ! -f cert.pem ]
 then
-	openssl req -newkey rsa:4096 -keyout key.pem -out cert.pem -days 10000 -nodes
+	openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 10000 -nodes
 fi
 
 ##########################################
