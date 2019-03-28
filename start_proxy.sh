7366bf488a23c0ef3fb06411b92ab9fe5aea75d0
huynhnham180793 <48785801+huynhnham180793@users.noreply.github.com>
2019-03-28 09:20:05 +0700

    Update start_proxy.sh

--- start_proxy.sh
+++ start_proxy.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-##########################################
-# xmrig-PLUGandPLAY (enWILLYado version) #
-##########################################
 
-##########################################
 # Certs
 
 if [ ! -f key.pem ] || [ ! -f cert.pem ]
 then
-	openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 10000 -nodes
+	openssl req -newkey rsa:4096 -keyout key.pem -out cert.pem -days 10000 -nodes
 fi
 
 ##########################################
@@ -17,5 +13,5 @@ fi
 
 if [ -f xmrig-proxy.exe ]
 then
-	./xmrig-proxy.exe --tls-cert=cert.pem --tls-cert-key=key.pem --tls-bind 0.0.0.0:443 -o pool.supportxmr.com:443 --tls -u 433hhduFBtwVXtQiTTTeqyZsB36XaBLJB6bcQfnqqMs5RJitdpi8xBN21hWiEfuPp2hytmf1cshgK5Grgo6QUvLZCP2QSMi
+	./xmrig-proxy.exe --tls-cert=cert.pem --tls-cert-key=key.pem --tls-bind 0.0.0.0:443 -o pool.supportxmr.com:443 --tls -u 42xEfQuFb8dVvo9P8nuTVg5YrcEXHp4i844q2FykBa6vYG2Qumnb6r5ZD71zfHZf3eY1wFqC8vnDc6DP3f3DQcmDRtnfCig
 fi
